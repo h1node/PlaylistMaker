1118bca8c14b66f100f600114a74aab564c621b0
Iskarel <h1node@yandex.ru>
2024-09-20 01:59:55 +0300

    Retrofit formatTrackTime showPlaceholderMessage

--- app/src/main/java/com/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/playlistmaker/SearchActivity.kt
@@ -6,56 +6,37 @@ import android.text.Editable
 import android.text.TextWatcher
 import android.view.MenuItem
 import android.view.View
+import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
-import android.widget.EditText
-import android.widget.ImageView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.playlistmaker.data.MusicApi
+import com.playlistmaker.data.itunesdb.ResultResponse
 import com.playlistmaker.databinding.ActivitySearchBinding
 import com.playlistmaker.view.rv_adapter.MusicRVAdapter
-
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class SearchActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivitySearchBinding
     private lateinit var adapter: MusicRVAdapter
-    private lateinit var searchEditText: EditText
     private var inputText: String = ""
-    private val musicDataBase = listOf(
-        Track(
-            "Smells Like Teen Spirit",
-            "Nirvana",
-            "5:01",
-            "https://is5-ssl.mzstatic.com/image/thumb/Music115/v4/7b/58/c2/7b58c21a-2b51-2bb2-e59a-9bb9b96ad8c3/00602567924166.rgb.jpg/100x100bb.jpg"
-        ),
-        Track(
-            "Billie Jean",
-            "Michael Jackson",
-            "4:35",
-            "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/3d/9d/38/3d9d3811-71f0-3a0e-1ada-3004e56ff852/827969428726.jpg/100x100bb.jpg"
-        ),
-        Track(
-            "Stayin' Alive",
-            "Bee Gees",
-            "4:10",
-            "https://is4-ssl.mzstatic.com/image/thumb/Music115/v4/1f/80/1f/1f801fc1-8c0f-ea3e-d3e5-387c6619619e/16UMGIM86640.rgb.jpg/100x100bb.jpg"
-        ),
-        Track(
-            "Whole Lotta Love",
-            "Led Zeppelin",
-            "5:33",
-            "https://is2-ssl.mzstatic.com/image/thumb/Music62/v4/7e/17/e3/7e17e33f-2efa-2a36-e916-7f808576cf6b/mzm.fyigqcbs.jpg/100x100bb.jpg"
-        ),
-        Track(
-            "Sweet Child O'Mine",
-            "Guns N' Roses",
-            "5:03",
-            "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg "
-        )
-    )
+    private var failedQuery: String? = null
+
+    private val retrofit = Retrofit.Builder()
+        .baseUrl("https://itunes.apple.com")
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    private val musicApi = retrofit.create(MusicApi::class.java)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -64,48 +45,98 @@ class SearchActivity : AppCompatActivity() {
         setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(
-                systemBars.left,
-                systemBars.top,
-                systemBars.right,
-                systemBars.bottom
-            )
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        binding.rvSearch.layoutManager = LinearLayoutManager(this)
-        adapter = MusicRVAdapter(musicDataBase) { track ->
-            Toast.makeText(this, "Clicked on track: ${track.trackName}", Toast.LENGTH_SHORT).show()
+        adapter = MusicRVAdapter { track ->
+            Toast.makeText(
+                this@SearchActivity,
+                "Clicked on track: ${track.trackName}",
+                Toast.LENGTH_SHORT
+            ).show()
         }
+        binding.rvSearch.layoutManager = LinearLayoutManager(this)
         binding.rvSearch.adapter = adapter
-
 
         setSupportActionBar(binding.toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.setHomeButtonEnabled(true)
 
+        binding.searchEditText.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                val query = binding.searchEditText.text.toString()
+                if (query.isNotEmpty()) {
+                    searchMusic(query)
+                } else {
+                    Toast.makeText(this, "Введите запрос", Toast.LENGTH_SHORT).show()
+                }
+                true
+            } else {
+                false
+            }
+        }
 
-        searchEditText = findViewById(R.id.searchEditText)
-        val clearButton = findViewById<ImageView>(R.id.clearIcon)
-        clearButton.setOnClickListener {
-            searchEditText.setText("")
-            clearButton.visibility = View.GONE
+        binding.clearIcon.setOnClickListener {
+            binding.searchEditText.setText("")
+            it.visibility = View.GONE
             hideKeyboard()
         }
 
-
-        val searchTextWatcher = object : TextWatcher {
+        binding.searchEditText.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                clearButton.visibility = clearButtonVisibility(s)
+                binding.clearIcon.visibility = clearButtonVisibility(s)
                 inputText = s?.toString() ?: ""
             }
 
             override fun afterTextChanged(s: Editable?) {}
+        })
 
+        binding.placeholderButton.setOnClickListener {
+            val query = failedQuery
+            if (query != null) searchMusic(query)
         }
-        searchEditText.addTextChangedListener(searchTextWatcher)
+
+        binding.placeholderButton.visibility = View.GONE
+    }
+
+    private fun searchMusic(query: String) {
+        failedQuery = query
+        val call = musicApi.getMusic(query)
+        call.enqueue(object : Callback<ResultResponse> {
+            override fun onResponse(
+                call: Call<ResultResponse>,
+                response: Response<ResultResponse>
+            ) {
+                if (response.isSuccessful) {
+                    val resultResponse = response.body()
+                    if (resultResponse != null && resultResponse.results.isNotEmpty()) {
+                        adapter.items = resultResponse.results
+                        adapter.notifyDataSetChanged()
+                        hidePlaceholderMessage()
+                    } else {
+                        showPlaceholderMessage(
+                            getString(R.string.nothing_was_found),
+                            R.drawable.music_error
+                        )
+                    }
+                } else {
+                    showPlaceholderMessage(
+                        getString(R.string.nothing_was_found),
+                        R.drawable.music_error
+                    )
+                }
+            }
+
+            override fun onFailure(call: Call<ResultResponse>, t: Throwable) {
+                showPlaceholderMessage(
+                    getString(R.string.connection_problem),
+                    R.drawable.internet_error
+                )
+            }
+        })
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -121,22 +152,38 @@ class SearchActivity : AppCompatActivity() {
     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
         super.onRestoreInstanceState(savedInstanceState)
         inputText = savedInstanceState.getString(EDIT_TEXT_KEY, "")
-        searchEditText.setText(inputText)
+        binding.searchEditText.setText(inputText)
     }
 
-
     private fun clearButtonVisibility(s: CharSequence?): Int {
-        return if (s.isNullOrEmpty()) {
-            View.GONE
-        } else {
-            View.VISIBLE
-        }
+        return if (s.isNullOrEmpty()) View.GONE else View.VISIBLE
     }
 
     private fun hideKeyboard() {
         val inputMethodManager =
             getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        inputMethodManager.hideSoftInputFromWindow(searchEditText.windowToken, 0)
+        inputMethodManager.hideSoftInputFromWindow(binding.searchEditText.windowToken, 0)
+    }
+
+    private fun showPlaceholderMessage(text: String, imageRes: Int) {
+        binding.placeholderMessage.text = text
+        binding.placeholderMessage.visibility = if (text.isEmpty()) View.GONE else View.VISIBLE
+        binding.placeholderImageInternet.visibility =
+            if (imageRes == R.drawable.internet_error) View.VISIBLE else View.GONE
+        binding.placeholderImageMusic.visibility =
+            if (imageRes != R.drawable.internet_error) View.VISIBLE else View.GONE
+        binding.placeholderButton.visibility = if (text.isEmpty()) View.GONE else View.VISIBLE
+
+        if (text.isNotEmpty()) {
+            Toast.makeText(applicationContext, text, Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun hidePlaceholderMessage() {
+        binding.placeholderMessage.visibility = View.GONE
+        binding.placeholderImageMusic.visibility = View.GONE
+        binding.placeholderImageInternet.visibility = View.GONE
+        binding.placeholderButton.visibility = View.GONE
     }
 
     companion object {
